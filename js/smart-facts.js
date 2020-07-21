9d4a0c440b2f4704d53b4331646ce0d5817332c8
Mohsin Razzaq <mohsinrazzaq25@gmail.com>
2020-07-21 23:44:54 +0500

    added smart-facts js on residential sector page

---
+++ js/smart-facts.js
@@ -0,0 +1,19 @@
+function funct10kw() {
+    document.getElementById("h6-1").innerHTML = "32";
+    document.getElementById("h6-3").innerHTML = "> 16,000 units per year";
+}
+
+function funct15kw() {
+    document.getElementById("h6-1").innerHTML = "48";
+    document.getElementById("h6-3").innerHTML = "> 24,000 units per year";
+}
+
+function funct20kw() {
+    document.getElementById("h6-1").innerHTML = "64";
+    document.getElementById("h6-3").innerHTML = ">32,000 units per year";
+}
+
+function funct25kw() {
+    document.getElementById("h6-1").innerHTML = "80";
+    document.getElementById("h6-3").innerHTML = ">  40,000 units per year";
+}
